51c5635e1cbf0c98ce7b60545093378a582b6d2b
tiko <tiko.pb@gmail.com>
2021-05-25 20:07:27 +0700

    start chapter 4

---
+++ basic.js
@@ -0,0 +1,14 @@
+//contoh penulisan variabel pada javascript 
+var alphabet ="ABC";
+let number =TryMehthod();
+alphabet = alphabet + number;
+console.log(alphabet);
+
+function TryMehthod(){
+    let numberSecond =12345;
+    return numberSecond;
+}
+
+//const example 
+const phi = 3.14;
+console.log(phi);
