9a99d3409e98d0b5cbd1ec33a4236013651233a9
tiko <tiko.pb@gmail.com>
2021-05-27 19:29:54 +0700

    zoom meeting 25 mei

--- basic.js
+++ basic.js
@@ -1,14 +1,46 @@
-//contoh penulisan variabel pada javascript 
-var alphabet ="ABC";
-let number =TryMehthod();
-alphabet = alphabet + number;
-console.log(alphabet);
 
-function TryMehthod(){
-    let numberSecond =12345;
-    return numberSecond;
+var namaWalikota = "Asep";
+
+function ChangePresidentName(nama){
+    let candidateName = nama;
+    return candidateName;
 }
 
-//const example 
-const phi = 3.14;
-console.log(phi);
+function changeVicePressidentName(nama){
+    let candidateViceName = nama;
+    return candidateViceName;
+}
+
+var returnValue = `${namaWalikota} merupakan walikota dari president `+ ChangePresidentName("JOKO")+` dan wakil President nya adalah `+ changeVicePressidentName("ANI");
+console.log(returnValue);
+
+var object = {
+    hello: 'world',
+    world: 'earth'
+}
+
+console.log(object.world);
+
+var array =[
+    "world", 23, "binar", "learn"
+]
+
+console.log(array)
+array.push("ocean") // tambah item 
+console.log(array) // cara pemanggilan array 
+array.pop() //menghapus array terakhir 
+console.log(array)
+array.shift() //menghapus array pertama 
+console.log(array)
+array.unshift(24) //menambah valud pertama di array 
+console.log(array)
+
+array.forEach(function(i){
+    console.log(i)
+})
+
+//fioltered array function
+var filteredArray = array.filter(function(i){
+    return i !=="binar"
+})
+console.log(filteredArray)
